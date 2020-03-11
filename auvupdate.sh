bab5f1ba3b487df2a718aa609a9fb9bd68ab1e19
Steve Haddock <shaddock@ucsc.edu>
2020-03-10 23:14:07 -0700

    removed SIM from cron

--- auvupdate.sh
+++ auvupdate.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-VEH='daphne pontus tethys galene sim triton makai'
+#VEH='daphne pontus tethys galene triton makai brizo sim'
+VEH='daphne pontus tethys galene triton makai'
 rm -f /var/www/html/widget/auv_*.svg
 for v in $VEH; do /var/www/html/widget/auvstatus.py -v $v -f; done
