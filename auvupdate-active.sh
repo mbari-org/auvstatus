49cfdebcc83eed13a8c83397a334705ab49298a8
Steve Haddock <shaddock@ucsc.edu>
2020-06-08 16:59:22 -0700

    made two shell script with different update rates

--- auvupdate-active.sh
+++ auvupdate-active.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 #VEH='daphne pontus tethys galene triton makai brizo sim'
-VEH='pontus tethys galene triton makai'
+VEH='daphne'
 
 for v in $VEH; do rm -f /var/www/html/widget/auv_$v.svg; /var/www/html/widget/auvstatus.py -v $v -f; done
+
