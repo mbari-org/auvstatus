07c6ac60c14dad79eed0524ec88f10be3538f32f
Steve Haddock <shaddock@ucsc.edu>
2020-10-07 22:24:57 -0700

    updated crontab scripts

--- auvupdate-active.sh
+++ auvupdate-active.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #VEH='daphne pontus tethys galene triton makai brizo sim'
-VEH='pontus tethys'
+VEH='makai pontus'
 
 #for v in $VEH; do rm -f /var/www/html/widget/auv_$v.svg; python2.7 /var/www/html/widget/auvstatus.py -v $v -f; done
 for v in $VEH; do python2.7 /var/www/html/widget/auvstatus.py -v $v -f; done
-#python2.7 /var/www/html/widget/esp_widget.py -v makai -f
+python2.7 /var/www/html/widget/esp_widget.py -v makai -f
--- auvupdate.sh
+++ auvupdate.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 #VEH='daphne pontus tethys galene triton makai brizo sim'
-VEH='daphne makai brizo galene triton'
+VEH='tethys daphne brizo galene triton'
 
 for v in $VEH; do rm -f /var/www/html/widget/auv_$v.svg; /var/www/html/widget/auvstatus.py -v $v -f; done
